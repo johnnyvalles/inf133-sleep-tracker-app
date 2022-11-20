77c37a7b3353c2efc74359f74f32183e6a199ca0
Johnny Valles <vallesja@uci.edu>
2022-11-20 01:57:28 -0800

    add setters, getters, utilities

--- src/app/types/overnight-sleep-log.ts
+++ src/app/types/overnight-sleep-log.ts
@@ -12,31 +12,33 @@ export class OvernightSleepLog extends SleepLog {
         this._notes = notes;
     }
 
-    get sleepStart(): Date {
-        return this._sleepStart;
-    }
-
+    // getters & setters
     set sleepStart(sleepStart: Date) {
         this._sleepStart = sleepStart;
     }
 
-    get sleepEnd(): Date {
-        return this._sleepEnd;
+    get sleepStart(): Date {
+        return this._sleepStart;
     }
 
     set sleepEnd(sleepEnd: Date) {
         this._sleepEnd = sleepEnd;
     }
 
-    get notes(): string {
-        return this._notes;
+    get sleepEnd(): Date {
+        return this._sleepEnd;
     }
 
     set notes(notes: string) {
         this._notes = notes;
     }
 
-    timeSleptMilliseconds(): number {
+    get notes(): string {
+        return this._notes;
+    }
+
+    // utilities
+    sleepDurationMilli(): number {
         // start time in milliseconds
         const startMs = this._sleepStart.getTime();
         // end time in milliseconds
@@ -45,14 +47,138 @@ export class OvernightSleepLog extends SleepLog {
         return endMs - startMs;
     }
 
-    get summaryString(): string {
-        const diffMs = this.timeSleptMilliseconds();
-        return Math.floor(diffMs / (1000*60*60)) + " hours, " + Math.floor(diffMs / (1000*60) % 60) + " minutes.";
+    sleepDurationHoursMins(): number[] {
+        const hours = Math.floor(this.sleepDurationMilli() / (1000 * 60 * 60));
+        const minutes = Math.floor(this.sleepDurationMilli() / (1000 * 60) % 60);
+        return [hours, minutes];
     }
 
-    get sleepDuration(): number[] {
-        const hours = Math.floor(this.timeSleptMilliseconds() / (1000*60*60));
-        const minutes = Math.floor(this.timeSleptMilliseconds() / (1000*60) % 60);
-        return [hours, minutes];
+    sleepDurationString(): string {
+        const duration: number[] = this.sleepDurationHoursMins();
+        let durationString: string = "";
+
+        durationString += duration[0];
+        if (duration[0] !== 1) {
+            durationString += " hrs, ";
+        } else {
+            durationString += " hr, ";
+        }
+
+        durationString += duration[1];
+        if (duration[1] !== 1) {
+            durationString += " mins";
+        } else {
+            durationString += " min";
+        }
+
+        return durationString;
+    }
+
+    formattedSleepStartStrings(): string[] {
+        const month: number = this._sleepStart.getMonth() + 1;
+        const day: number = this._sleepStart.getDate();
+        const year: number = this._sleepStart.getFullYear();
+        let numerical = "";
+        
+        numerical += (month < 10 ? "0" + month : month);
+        numerical += "/";
+        numerical += (day < 10 ? "0" + day : day);
+        numerical += "/";
+        numerical += year;
+
+        const options = {
+            month: "long",
+            day: "numeric",
+            year: "numeric"
+        } as const;
+
+        const locale: string = "en-US";
+
+        return [numerical, this._sleepStart.toLocaleString(locale, options)];
+    }
+
+    formattedSleepEndStrings(): string[] {
+        const month: number = this._sleepEnd.getMonth() + 1;
+        const day: number = this._sleepEnd.getDate();
+        const year: number = this._sleepEnd.getFullYear();
+        let numerical = "";
+        
+        numerical += (month < 10 ? "0" + month : month);
+        numerical += "/";
+        numerical += (day < 10 ? "0" + day : day);
+        numerical += "/";
+        numerical += year;
+
+        const options = {
+            month: "long",
+            day: "numeric",
+            year: "numeric"
+        } as const;
+
+        const locale: string = "en-US";
+
+        return [numerical, this._sleepEnd.toLocaleString(locale, options)];
+    }
+
+    sleepStartDayOfWeek(): string {
+        const options = {
+            weekday: "long"
+        } as const;
+        const locale: string = "en-US";
+
+        return this._sleepStart.toLocaleString(locale, options);
+    }
+
+    sleepEndDayOfWeek(): string {
+        const options = {
+            weekday: "long"
+        } as const;
+        const locale: string = "en-US";
+
+        return this._sleepEnd.toLocaleString(locale, options);
+    }
+
+    sleepStartMonth(): string {
+        const options = {
+            month: "long"
+        } as const;
+        const locale: string = "en-US";
+
+        return this._sleepStart.toLocaleString(locale, options);
+    }
+
+    sleepEndMonth(): string {
+        const options = {
+            month: "long"
+        } as const;
+        const locale: string = "en-US";
+
+        return this._sleepEnd.toLocaleString(locale, options);
+    }
+
+    sleepStartYear(): string {
+        return "" + this._sleepStart.getFullYear();
+    }
+
+    sleepEndYear(): string {
+        return "" + this._sleepEnd.getFullYear();
+    }
+
+    sleepStartDay(): string {
+        const options = {
+            day: "numeric"
+        } as const;
+        const locale: string = "en-US";
+
+        return this._sleepStart.toLocaleString(locale, options);
+    }
+
+    sleepEndDay(): string {
+        const options = {
+            day: "numeric"
+        } as const;
+        const locale: string = "en-US";
+
+        return this._sleepEnd.toLocaleString(locale, options);
     }
 }
