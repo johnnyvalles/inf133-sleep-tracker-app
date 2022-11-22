413fb95b7965044176b1316fbe017c83254e2615
Johnny Valles <vallesja@uci.edu>
2022-11-21 23:38:18 -0800

    remove created date

--- src/app/types/sleep-log.ts
+++ src/app/types/sleep-log.ts
@@ -2,21 +2,8 @@ import { generate } from 'shortid';
 
 export class SleepLog {
     public id?: string;
-    public created: Date;
 
     constructor() {
         this.id = generate();
-        this.created = new Date();
-    }
-    
-    getCreatedString(): string {
-        const locale = "en-US";
-        const options = {
-            weekday: "long",
-            month: "long",
-            day: "numeric"
-        } as const;
-
-        return this.created.toLocaleDateString(locale, options);
     }
 }
