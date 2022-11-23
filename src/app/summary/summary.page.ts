bfeb64f8d73fb6388a964783212df1a4c648d5f7
Johnny Valles <vallesja@uci.edu>
2022-11-23 13:28:57 -0800

    create summary page

--- src/app/summary/summary.page.ts
+++ src/app/summary/summary.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SleepStorageService } from '../services/sleep-storage.service';
+import { SleepinessLog } from '../types/sleepiness-log';
+
 
 @Component({
   selector: 'app-summary',
@@ -7,6 +10,65 @@ import { Component } from '@angular/core';
 })
 export class SummaryPage {
 
-  constructor() {}
+  public overnightTotal: number = 0;
+  public sleepinessTotal: number = 0;
+  public averageDuration: string = "";
+  public averageSleepiness: number = 0;
+  public values = SleepinessLog.SCALE_DESCRIPTIONS;
 
+  constructor(
+    private sleepStorageService: SleepStorageService
+  ) {}
+
+  async ionViewDidEnter() {
+    this.overnightTotal = 0;
+    this.sleepinessTotal = 0;
+
+    let data: any= await this.sleepStorageService.getOvernightSleepLogs();
+    if (data) {
+      this.overnightTotal = data.length;
+      let duration = 0;
+      for (let log of data) {
+        duration += log.sleepDurationMilli();
+      }  
+      duration /= data.length;
+      this.averageDuration = this.sleepDurationHoursMinsString(duration);
+    }
+
+    data = await this.sleepStorageService.getSleepinessLogs();
+    if (data) {
+      this.sleepinessTotal = data.length;
+
+      let value = 0;
+      for (let log of data) {
+        value += log.sleepiness;
+      }  
+      value /= data.length;
+  
+      this.averageSleepiness = Math.floor(value);
+    }
+  }
+
+
+  sleepDurationHoursMinsString(duration: number) {
+    const hours = Math.floor(duration / (1000 * 60 * 60));
+    const minutes = Math.floor(duration / (1000 * 60) % 60);
+
+    let durationString: string = "";
+    durationString += hours;
+    if (hours !== 1) {
+        durationString += " hrs, ";
+    } else {
+        durationString += " hr, ";
+    }
+
+    durationString += minutes;
+    if (minutes !== 1) {
+        durationString += " mins";
+    } else {
+        durationString += " min";
+    }
+
+    return durationString;
+}
 }
